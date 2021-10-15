8e05064ff91a716d5be1afc6a67f1f8b57d99949
alconor <alconor23@hotmail.com>
2021-10-14 21:30:28 -0500

    Se agrego grupo a la consulta de vsqlcss

--- models/models_reports.py
+++ models/models_reports.py
@@ -30,16 +30,13 @@ class JC_vsqlcss(models.Model):
     documento_inventario = fields.Char(string='Documento Inventario', readony=True)
     linea = fields.Integer(string='Linea', readonly=True)
     articulo = fields.Char(string='Articulo',readonly=True)
-    bodega_id = fields.Many2one('stock_warehouse', readonly=True, string='Bodega')
     bodega = fields.Char(string='Bodega',readonly=True)
     cantidad = fields.Float(string='Cantidad', readonly=True)
     ajuste_configurable = fields.Char(string='Ajueste Configurable', readonly=True)
     tipo = fields.Char(string='Tipo', readonly=True)
     subtipo = fields.Char(string='SubTipo', readonly=True)
     subsubtipo = fields.Char(string='SubSubTipo', readonly=True)
-    centro_costo_id = fields.Many2one('account_analytic_account', readonly=True, string='Centro Costo')
     centro_costo = fields.Char(string='Centro Costo',readonly=True)
-    fase_id = fields.Many2one('project_task_phase', readonly=True, string='Fase')
     fase = fields.Char(string='Fase',readonly=True)
     cuenta_contable = fields.Char(string='Cuenta Contable', readonly=True)
     orden_cambio = fields.Char(string='Orden Cambio', readonly=True)
@@ -70,7 +67,8 @@ ptp."name" as fase,
 '1' as orden_cambio,
 sm.note as notes,
 1 as company_id,
-sm."date" as fecha
+sm."date" as fecha,
+pro.name as proyecto
 from public.stock_move sm  left join
 public.product_product pp 
 on sm.product_id = pp.id left join 
@@ -80,8 +78,19 @@ stock_location sl2
 on sl.location_id = sl2.id left join
 account_analytic_account aaa 
 on sm.analytic_account_id = aaa.id left join 
-project_task_phase ptp 
-on sm.phase_id = ptp.id );
+project_task_phase ptp
+on sm.phase_id = ptp.id left join
+project_project pro
+on ptp.project_id = pro.id
+group by sm."name",
+pp.default_code ,
+sl2."name" ,
+sm.product_qty ,
+aaa.code ,
+ptp.name,
+sm.note ,
+sm."date" ,
+pro."name" );
         """
         self.env.cr.execute(query)
 
