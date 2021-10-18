bde2176982c0fec39929174df5ca4692da3c29b9
alconor <alconor23@hotmail.com>
2021-10-18 17:42:45 -0500

    Agregando campos: bodega_destinoi y origen, referencia, tipo_recoleccion y negatvo en las devoluciones

--- models/models_reports.py
+++ models/models_reports.py
@@ -45,18 +45,22 @@ class JC_vsqlcss(models.Model):
     company_id = fields.Many2one(
         'res.company', string='Company', readonly=True)
     fecha = fields.Datetime(string='Fecha', readonly=True)
+    bodega_origen = fields.Char(string='Bodega Origen',readonly=True)
+    bodega_destino = fields.Char(string='Bodega Destino',readonly=True)
+    referencia = fields.Char(string='Ref.',readonly=True)
+    tipo_recoleccion = fields.Char(string='Tipo Recoleccion',readonly=True)
 
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
-        query = """CREATE OR REPLACE VIEW project_vsqlcss AS (select row_number() OVER (PARTITION BY true) as id,
+        query = """CREATE OR REPLACE VIEW project_vsqlcss AS ( select row_number() OVER (PARTITION BY true) as id,
 True as active,
 sm.name, 
 'ZU' as paquete_inventario,
 'CON-000' as documento_inventario,
 row_number() OVER (PARTITION BY true) as linea,
 pp.default_code as articulo,
-sl2."name" as bodega,
-sm.product_qty as cantidad,
+case when position('OUT' in sm.reference) > 0 then sl2."name" when position('DEV' in sm.reference) > 0 then slo2."name" else '' end  as bodega,
+sm.product_qty*(case when position('OUT' in sm.reference) > 0 then +1 when position('DEV' in sm.reference) > 0 then -1 else 0 end) as cantidad,
 '~CC~' as ajuste_configurable,
 'C' as tipo,
 'D' as subtipo,
@@ -68,7 +72,11 @@ ptp."name" as fase,
 sm.note as notes,
 1 as company_id,
 sm."date" as fecha,
-pro.name as proyecto
+pro.name as proyecto,
+sl."complete_name" as bodega_origen,
+slo."complete_name" as bodega_destino,
+sm.reference as referencia,
+spt."name" as tipo_recoleccion
 from public.stock_move sm  left join
 public.product_product pp 
 on sm.product_id = pp.id left join 
@@ -81,7 +89,14 @@ on sm.analytic_account_id = aaa.id left join
 project_task_phase ptp
 on sm.phase_id = ptp.id left join
 project_project pro
-on ptp.project_id = pro.id
+on ptp.project_id = pro.id left join
+stock_location slo 
+on sm.location_dest_id = slo.id left join 
+stock_location slo2
+on slo.location_id = slo2.id left join 
+stock_picking_type spt 
+on sm.picking_type_id = spt.id 
+where NOT(sm.picking_type_id is null )
 group by sm."name",
 pp.default_code ,
 sl2."name" ,
@@ -90,7 +105,14 @@ aaa.code ,
 ptp.name,
 sm.note ,
 sm."date" ,
-pro."name" );
+pro."name" ,
+sm.location_dest_id ,
+sm.picking_type_id ,
+slo2."name",
+sm.reference ,
+spt."name",
+sl.complete_name,
+slo.complete_name );
         """
         self.env.cr.execute(query)
 
